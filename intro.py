bb52f2c57dd772467f0292702c4e6602ee56776d
Aditya <35306205+adi0900@users.noreply.github.com>
2024-10-30 19:09:50 +0530

    Initial commit

---
+++ intro.py
@@ -0,0 +1,9 @@
+# Programming Language - Speak in 1 and 0's 
+# Python is Interpreter 
+# Cpython (.py) -> Interprets codes in Bytecode ->  Cpython VM to Binary
+'''
+print("Hello World")
+input("What is your name? ")
+'''
+name = input("What is your Name? ")
+print("My Name is " + name)
